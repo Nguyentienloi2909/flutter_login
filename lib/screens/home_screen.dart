9d5b09413e30c71bdda75e1382eefbc894ea6bf4
Nguyentienloi2909 <121503395+Nguyentienloi2909@users.noreply.github.com>
2023-03-01 21:33:05 +0700

    update

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_login_1/screens/sigin_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Logout"),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => SignInScreen()));
+          },
+        ),
+      ),
+    );
+  }
+}
